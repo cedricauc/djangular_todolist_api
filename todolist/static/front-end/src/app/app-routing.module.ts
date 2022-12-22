9e99cbcec9e195473d2898cec3744502141d24b2
Cedric Auclair <39864243+cedricauc@users.noreply.github.com>
2022-12-22 15:26:36 +0100

    add components, interface, routing

--- todolist/static/front-end/src/app/app-routing.module.ts
+++ todolist/static/front-end/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {TaskComponent} from "./components/task/task.component";
+import {PageNotFoundComponent} from "./components/pagenotfound/pagenotfound.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'task/create', component: TaskComponent },
+      { path: 'task/edit/:id', component: TaskComponent },
+      { path: 'task/:id', component: TaskComponent },
+      { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- todolist/static/front-end/src/app/app.module.ts
+++ todolist/static/front-end/src/app/app.module.ts
@@ -2,17 +2,45 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { TaskComponent } from './components/task/task.component';
+import { HomeComponent } from './components/home/home.component';
+import { TaskEditComponent } from './components/task/task-edit.component';
+import { TaskListComponent } from './components/task/task-list.component';
+import { TaskSearchComponent } from './components/task/task-search.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.component';
+import {CommonModule} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    HomeComponent,
+    TaskEditComponent,
+    TaskListComponent,
+    TaskSearchComponent,
+    NavmenuComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+      { provide: 'BASE_URL', useFactory: getBaseUrl }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function getBaseUrl() {
+    return document.getElementsByTagName('base')[0].href;
+}
+
