95490ee74a5c2caa7050adcb2b883f50806b7511
Cedric Auclair <39864243+cedricauc@users.noreply.github.com>
2022-12-22 14:52:21 +0100

    resful api and deploy angular in static repo

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+     A viewset for viewing and editing task instances.
+     """
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+def home(request):
+    return render(request, "angular_index.html", {})
